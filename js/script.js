a407bd9775c02dd7872866c5b2b7f3366681c8cb
adrian <adrian.gollmann@gmail.com>
2019-11-07 12:06:17 +0100

    Update html Add css Update js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,53 @@
-window.onload = function () {
 
-    var obj = document.getElementById('btn1');
-    console.log(obj);
+function sortTable(dir) {
+  var table, rows, switching, i, x, y, shouldSwitch
+  table = document.getElementById('worldData')
+  switching = true
 
-    obj.addEventListener("click", function () {
-        alert(obj.id);
-    });
+  while (switching) {
+    switching = false
+    rows = table.rows
 
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false
+      x = rows[i].getElementsByTagName('TD')[1]
+      y = rows[i + 1].getElementsByTagName('TD')[1]
+
+      if (dir===1) {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true
+          break
+        }
+      } else if (dir===0) {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true
+          break
+        }
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+      switching = true
+    }
+  }
+}
+
+function toggleRow(cls, id) {
+  var lst = document.getElementsByClassName(cls);
+  var value = document.getElementById(id).value
+  console.log(id)
+  console.log(this.value)
+  if (value==='show') {
+    for(var i = 0; i < lst.length; ++i) {
+      console.log(1)
+      lst[i].style.display = 'none';
+    }
+    document.getElementById(id).value = 'hide'
+  } else if (value==='hide') {
+    for(var i = 0; i < lst.length; ++i) {
+      lst[i].style.display = '';
+    }
+    document.getElementById(id).value = 'show'
+  }
+  
 }
