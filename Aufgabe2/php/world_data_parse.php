2118789faeedd8242e75aa998e4c64d02a925ebe
adrian <adrian.gollmann@gmail.com>
2019-11-19 14:08:57 +0100

    prepare php

--- Aufgabe2/php/world_data_parse.php
+++ Aufgabe2/php/world_data_parse.php
@@ -3,6 +3,17 @@
 // PHP file for basic class with main functions
 class WorldDataParser {
 
+    func parseCSV(string $path) {
+
+    }
+
+    func saveXML(array $data) {
+
+    }
+
+    func printXML(string $xmlpath, string $xsltpath) {
+
+    }
     
 }
 ?>
